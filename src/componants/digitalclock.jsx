828d6337225d666cdec14d92587b2522d1d7e046
Prathamesh <prathameshpatil51514@gmail.com>
2025-01-20 09:50:18 +0530

    Adding responsiveness,improve in styling

--- src/componants/digitalclock.jsx
+++ src/componants/digitalclock.jsx
@@ -9,7 +9,7 @@ export const Digitalclock = () => {
       setTime(new Date());
     }, 1000);
 
-    return () => clearInterval(interval);
+    
   }, []);
 
   const toggleFormat = () => {
@@ -23,14 +23,14 @@ export const Digitalclock = () => {
   const currentDayIndex = time.getDay(); 
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-300 via-indigo-400 to-purple-500 text-white">
-      {/* Title */}
-      <h1 className="text-4xl font-extrabold mb-4 animate-pulse">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-300 via-indigo-400 to-purple-500 text-white">
+     
+      <h1 className="text-4xl font-extrabold mb-4 ">
         Digital Clock
       </h1>
 
       
-      <button
+      <button type="button"
         onClick={toggleFormat}
         className="mb-6 px-6 py-3 bg-gradient-to-r from-teal-400 to-teal-600 text-white font-semibold rounded-full shadow-md hover:shadow-lg transform hover:scale-105 transition-all duration-300 ease-in-out"
       >
@@ -40,9 +40,9 @@ export const Digitalclock = () => {
       
       <div className="bg-white text-gray-800 rounded-lg shadow-lg px-12 py-8 text-center transform hover:scale-105 transition-all duration-300 ease-in-out">
        
-        <div className="flex justify-between text-gray-500 mb-4 text-lg uppercase tracking-wide">
+        <div className="flex justify-between gap-4 text-gray-500 mb-4 text-lg uppercase tracking-wide ">
           {daysOfWeek.map((day, index) => (
-            <span
+            <span key={day}
               
               className={`${
                 currentDayIndex === (index + 1)  ? "underline text-black" : ""
@@ -64,11 +64,11 @@ export const Digitalclock = () => {
         </div>
 
         
-        {!is24HourFormat && (
+        {/* {!is24HourFormat && (
           <div className="text-gray-600 text-lg mt-2">
             {time.getHours() >= 12 ? "PM" : "AM"}
           </div>
-        )}
+        )} */}
 
         
         <div className="text-gray-500 text-sm mt-4">
